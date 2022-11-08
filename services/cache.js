464f47b2d4d488156c803dd3d6948fcc65359108
Mridul <mit.ash.619@gmail.com>
2022-11-09 03:38:53 +0530

    show key for redis

--- services/cache.js
+++ services/cache.js
@@ -4,6 +4,7 @@ const exec = mongoose.Query.prototype.exec;
 
 mongoose.Query.prototype.exec = function () {
   console.log('query about to run');
-
+  const key = { ...this.getQuery(), collection: this.mongooseCollection.name };
+  console.log(key);
   return exec.apply(this, arguments);
 };
