25dbb3e2f1c9444069e43ee135a28ba71461b484
Mridul <mit.ash.619@gmail.com>
2022-11-16 05:59:40 +0530

    hydrating models

--- services/cache.js
+++ services/cache.js
@@ -1,10 +1,30 @@
 const mongoose = require('mongoose');
+const redis = require('redis');
+
+const redisUrl = 'redis://127.0.0.1:6379';
+const client = redis.createClient(redisUrl);
+client.connect();
 
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.exec = function () {
-  console.log('query about to run');
-  const key = { ...this.getQuery(), collection: this.mongooseCollection.name };
-  console.log(key);
-  return exec.apply(this, arguments);
+mongoose.Query.prototype.exec = async function () {
+  const key = JSON.stringify({
+    ...this.getQuery(),
+    collection: this.mongooseCollection.name,
+  });
+
+  const cacheValue = await client.get(key);
+
+  if (cacheValue) {
+    const doc = JSON.parse(cacheValue);
+    return Array.isArray(doc)
+      ? doc.map(d => new this.model(d))
+      : new this.model(doc);
+  }
+
+  const result = await exec.apply(this, arguments);
+
+  client.set(key, JSON.stringify(result));
+
+  return result;
 };
